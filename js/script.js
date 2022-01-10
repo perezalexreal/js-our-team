108648c0bb595a3d0615a616c56ad1699eeb0210
perezalexreal <perezalexreal@gmail.com>
2022-01-10 17:29:08 +0100

    Finito

--- js/script.js
+++ js/script.js
@@ -31,3 +31,33 @@ const team = [
     },
   ];
   
+let container = document.querySelector('.team-container');
+
+for(let i = 1; i < team.length; i++){
+  //creo elementi 
+let teamcard = document.createElement('div');
+let cardimage = document.createElement('div');
+let cardtext = document.createElement('div');
+let h3 = document.createElement('h3');
+let paragraph = document.createElement('p');
+let img = document.createElement('img');
+// assegno classi a elementi
+
+teamcard.classList.add('team-card');
+cardimage.classList.add('card-image');
+cardtext.classList.add('card-text');
+
+// aggiungo valori ad elementi 
+img.src =  `img/${team[i]['image']}`
+h3.textContent = team[i]['name'];
+paragraph.textContent = team[i]['role'];
+
+// aggiungo elementi html in ordine 
+
+container.append(teamcard);
+teamcard.append(cardimage);
+cardimage.append(img)
+teamcard.append(cardtext);
+cardtext.append(h3);
+cardtext.append(paragraph);
+}
